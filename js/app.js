c633835d89701f7b4966771307a7381dc14e417a
chidiebereojingwa <ojingwaoji@gmail.com>
2020-08-27 21:24:34 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//Variables
+
+const courses = document.querySelector('#courses-list')
+
+
+
+//Listeners
+
+loadEventListeners();
+
+function loadEventListeners() {
+    // When a new course is added
+    courses.addEventListener('click',buyCourse)
+
+}
+
+
+
+
+//Functions
+
+function buyCourse(e) {
+    e.preventDefault()
+   // Use delegation to find the couse that was added
+    if(e.target.classList.contains('add-to-cart')){
+        // read the course values
+        const course = e.target.parentElement.parentElement;
+        // read the values
+        getCourseInfo(course);
+    }
+
+}
+// Reads the HTML information of the selected course
+function getCourseInfo(course) {
+    console.log(course)
+}
